d3b74c3bd3de30418aa034b7113065e7caa83b5b
VishalChoudhary <vishalghn953@gmail.com>
2023-04-08 18:14:47 +0530

    footer update commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
     <Box mt="80px" bgcolor="#fdecdc">
         <Stack gap="40px" sx={{ alignItems: 'center', justifyContent: 'center' }} direction="row" px="40px" pt="24px">
             <img className='footer' src={Logo} alt="logo" style={{ width: '190px', height: '150px', marginTop: '-24px' }} />
-            <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">© atheistcoder</Typography>
+            <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">©atheistcoder</Typography>
         </Stack>
     </Box>
 );
